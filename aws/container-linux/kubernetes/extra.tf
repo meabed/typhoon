fc6d293b778625ae4a20e66b77eab012a7d2292b
Mohamed Meabed <mo.meabed@gmail.com>
2020-08-10 18:39:34 +0400

    chore: update nlp ip to eip

---
+++ aws/container-linux/kubernetes/extra.tf
@@ -0,0 +1,23 @@
+// usage az-zone-match = ["*az1"]
+variable "az-zone-match" {
+  default = ["eu-west-1a"]
+}
+
+variable "subnet_size" {
+  default = 2
+}
+
+// extra resources
+resource "aws_eip" "nlp_eip" {
+  vpc = true
+  associate_with_private_ip = cidrhost(aws_subnet.public.0.cidr_block, 1)
+}
+
+// outputs
+output "aws_eip_nlp_public_ip" {
+  value = aws_eip.nlp_eip.public_ip
+}
+
+output "aws_eip_nlp_private_ip" {
+  value = aws_eip.nlp_eip.private_ip
+}
--- aws/container-linux/kubernetes/override.tf
+++ aws/container-linux/kubernetes/override.tf
@@ -17,12 +17,18 @@ resource "aws_subnet" "public" {
   // Increase number of IPV4 to fix Calico IP-Pool exhaust issue
   count = var.subnet_size
   availability_zone = data.aws_availability_zones.all.names[0]
+  // 10.0.3.0/16 => (2) 10.0.3.0/18 & 10.0.3.0/18
   cidr_block        = cidrsubnet(var.host_cidr, 2, count.index)
 }
 
+
 # Network Load Balancer for apiservers and ingress
 resource "aws_lb" "nlb" {
-  subnets = [aws_subnet.public.0.id]
+  subnets = null
+  subnet_mapping {
+    subnet_id = aws_subnet.public.0.id
+    allocation_id = aws_eip.nlp_eip.id
+  }
 }
 
 resource "aws_route_table_association" "public" {
